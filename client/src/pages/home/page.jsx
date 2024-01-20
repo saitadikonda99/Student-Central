c8f4c06b4ad8683ec14cd9f6c5528e7135d711d5
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2024-01-21 00:45:33 +0530

    did timepass with footer

---
+++ client/src/pages/home/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './page.css';
+
+const page = () => {
+  return (
+    <div className='home'>
+        <div className="home-in">
+            <h1>Home</h1>
+        </div>
+    </div>
+  )
+}
+
+export default page
