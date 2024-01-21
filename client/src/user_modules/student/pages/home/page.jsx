31a39ef72ad3cc1ea297095fbdcc887f9378c5ee
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2024-01-21 14:28:25 +0530

    minor improvements in the student view

--- client/src/user_modules/student/pages/home/page.jsx
+++ client/src/user_modules/student/pages/home/page.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+
+import useAuth from '../../../../hooks/UseAuth';
+import './page.css';
 
 const page = () => {
+  const { auth } = useAuth();
+
   return (
     <div className='stundent-home'>
       <div className="student-home-in">
-          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, et?</h1>
+        <div className="student-home-in-one">
+          <div className="student-home-in-one-in">
+            <h1>Dear, {auth.user}</h1>
+            <h1>Welcome to Student Activity Center Student Portal</h1>
+            <p>This enterprise is a application aimed to serve as a single platform for deal with the all kinds gtienvances of students. This portal is Designed and Developed by ZeroOne Code Club and maintained by first year students from the club</p>
+          </div>
+        </div>
+
       </div>
     </div>
   )
