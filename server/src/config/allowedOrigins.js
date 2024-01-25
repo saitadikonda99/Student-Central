fc119ca7813a14993759bc2101fe6334543e8789
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-01-25 23:02:36 +0530

    .gitignore updated

--- server/src/config/allowedOrigins.js
+++ server/src/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
-    "http://68.183.93.40:3521",
+    "http://68.183.93.40:80",
+    "http://localhost:3000",
+    "http://localhost:3001",
     "http://68.183.93.40:8891",    
-    "*",
 ];
 
 module.exports = allowedOrigins;
