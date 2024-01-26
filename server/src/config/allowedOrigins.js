1a14d320ada2dc4e8d0e894789644b314594a727
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-01-26 18:58:07 +0530

    view log added

--- server/src/config/allowedOrigins.js
+++ server/src/config/allowedOrigins.js
@@ -1,7 +1,10 @@
 const allowedOrigins = [
     "http://68.183.93.40:80",
+<<<<<<< HEAD
     "http://localhost:3000",
     "http://localhost:3001",
+=======
+>>>>>>> 19421bd (undefined updated again)
     "http://68.183.93.40:8891",    
 ];
 
