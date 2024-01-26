162191ac36024a5ac32caf9b6846fac74330ed15
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-01-26 19:22:50 +0530

    Conflicts resolved

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react()]
-=======
   plugins: [react()],
-	server: {
-    host: '68.183.93.40', // Replace with your local IP address
-    port: 80, // Set the port to your desired value
-  },
->>>>>>> 19421bd (undefined updated again)
 })
+
